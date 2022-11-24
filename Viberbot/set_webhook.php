a3d7d074f8b0112c5dec776cb7dbc14ac2602185
16051997551 <118951315+16051997551@users.noreply.github.com>
2022-11-24 10:16:09 +0700

    Update set_webhook.php

--- Viberbot/set_webhook.php
+++ Viberbot/set_webhook.php
@@ -1,6 +1,6 @@
 <?php
-	$auth_token = 'AUTH_TOKEN';
-	$webhook = 'WEBHOOK_URL';
+	$auth_token = '5029d80b0c27de55-b0d0c13054d9806e-91aff69b3a85683c';
+	$webhook = 'beauty-shop-life';
 	
 	$jsonData = 
 	'{
